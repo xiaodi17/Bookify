1cc407e08f313b57875f8d91c44d8d4dfdad0a05
David <xiaodi.david@gmail.com>
2024-02-01 23:23:51 +1000

    reserve booking

---
+++ src/Bookify.Application/Abstractions/Bookings/ReserveBooking/ReserveBookingCommand.cs
@@ -0,0 +1,9 @@
+using Bookify.Application.Abstractions.Messages;
+
+namespace Bookify.Application.Abstractions.Bookings.ReserveBooking;
+
+public sealed record ReserveBookingCommand(
+    Guid ApartmentId,
+    Guid UserId,
+    DateOnly StartDate,
+    DateOnly EndDate) : ICommand<Guid>;
