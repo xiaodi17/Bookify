884d43de50da6d89100ddf7c6ffe6f8e81540b2c
David <xiaodi.david@gmail.com>
2024-02-18 17:37:46 +1000

    refactor

--- src/Bookify.Api/Controllers/Users/UsersController.cs
+++ src/Bookify.Api/Controllers/Users/UsersController.cs
@@ -2,6 +2,7 @@ using Bookify.Application.Users.GetLoggedInUser;
 using Bookify.Application.Users.LogInUser;
 using Bookify.Application.Users.RegisterUser;
 using Bookify.Domain.Abstractions;
+using Bookify.Infrastructure.Authorization;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -20,7 +21,7 @@ public class UsersController : ControllerBase
     }
 
     [HttpGet("me")]
-    [Authorize(Roles = Roles.Registered)]
+    [HasPermission(Permissions.UsersRead)]
     public async Task<IActionResult> GetLoggedInUser(CancellationToken cancellationToken)
     {
         var query = new GetLoggedInUserQuery();
---
+++ src/Bookify.Api/Permissions.cs
@@ -0,0 +1,6 @@
+namespace Bookify.Api;
+
+internal static class Permissions
+{
+    public const string UsersRead = "users:read";
+}
