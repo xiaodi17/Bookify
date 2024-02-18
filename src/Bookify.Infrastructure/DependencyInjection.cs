f142f092ea5ad177ec3e84edec5c72b4becba3a7
David <xiaodi.david@gmail.com>
2024-02-18 16:54:01 +1000

    role migration script

--- src/Bookify.Infrastructure/DependencyInjection.cs
+++ src/Bookify.Infrastructure/DependencyInjection.cs
@@ -27,40 +27,24 @@ public static class DependencyInjection
         IConfiguration configuration)
     {
         services.AddTransient<IDateTimeProvider, DateTimeProvider>();
+
         services.AddTransient<IEmailService, EmailService>();
 
         AddPersistence(services, configuration);
 
+        // AddCaching(services, configuration);
+
         AddAuthentication(services, configuration);
 
+        // AddAuthorization(services);
+
+        // AddHealthChecks(services, configuration);
+
+        // AddApiVersioning(services);
+
+        // AddBackgroundJobs(services, configuration);
+
         return services;
-    }
-
-    private static void AddAuthentication(IServiceCollection services, IConfiguration configuration)
-    {
-        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer();
-
-        services.Configure<AuthenticationOptions>(configuration.GetSection("Authentication"));
-        services.ConfigureOptions<JwtBearerOptionsSetup>();
-        services.Configure<KeycloakOptions>(configuration.GetSection("Keycloak"));
-
-        services.AddTransient<AdminAuthorizationDelegatingHandler>();
-
-        services.AddHttpClient<IAuthenticationService, AuthenticationService>((serviceProvider, httpClient) =>
-            {
-                KeycloakOptions keycloakOptions = serviceProvider.GetRequiredService<IOptions<KeycloakOptions>>().Value;
-
-                httpClient.BaseAddress = new Uri(keycloakOptions.AdminUrl);
-            })
-            .AddHttpMessageHandler<AdminAuthorizationDelegatingHandler>();
-
-        services.AddHttpClient<IJwtService, JwtService>((serviceProvider, httpClient) =>
-        {
-            KeycloakOptions keycloakOptions = serviceProvider.GetRequiredService<IOptions<KeycloakOptions>>().Value;
-
-            httpClient.BaseAddress = new Uri(keycloakOptions.TokenUrl);
-        });
     }
 
     private static void AddPersistence(IServiceCollection services, IConfiguration configuration)
@@ -77,6 +61,8 @@ public static class DependencyInjection
 
         services.AddScoped<IBookingRepository, BookingRepository>();
 
+        // services.AddScoped<IReviewRepository, ReviewRepository>();
+
         services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<ApplicationDbContext>());
 
         services.AddSingleton<ISqlConnectionFactory>(_ =>
@@ -84,4 +70,95 @@ public static class DependencyInjection
 
         SqlMapper.AddTypeHandler(new DateOnlyTypeHandler());
     }
+
+    private static void AddAuthentication(IServiceCollection services, IConfiguration configuration)
+    {
+        services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer();
+
+        services.Configure<AuthenticationOptions>(configuration.GetSection("Authentication"));
+
+        services.ConfigureOptions<JwtBearerOptionsSetup>();
+
+        services.Configure<KeycloakOptions>(configuration.GetSection("Keycloak"));
+
+        services.AddTransient<AdminAuthorizationDelegatingHandler>();
+
+        services.AddHttpClient<IAuthenticationService, AuthenticationService>((serviceProvider, httpClient) =>
+        {
+            KeycloakOptions keycloakOptions = serviceProvider.GetRequiredService<IOptions<KeycloakOptions>>().Value;
+
+            httpClient.BaseAddress = new Uri(keycloakOptions.AdminUrl);
+        })
+        .AddHttpMessageHandler<AdminAuthorizationDelegatingHandler>();
+
+        services.AddHttpClient<IJwtService, JwtService>((serviceProvider, httpClient) =>
+        {
+            KeycloakOptions keycloakOptions = serviceProvider.GetRequiredService<IOptions<KeycloakOptions>>().Value;
+
+            httpClient.BaseAddress = new Uri(keycloakOptions.TokenUrl);
+        });
+
+        services.AddHttpContextAccessor();
+
+        services.AddScoped<IUserContext, UserContext>();
+    }
+
+    // private static void AddAuthorization(IServiceCollection services)
+    // {
+    //     services.AddScoped<AuthorizationService>();
+    //
+    //     services.AddTransient<IClaimsTransformation, CustomClaimsTransformation>();
+    //
+    //     services.AddTransient<IAuthorizationHandler, PermissionAuthorizationHandler>();
+    //
+    //     services.AddTransient<IAuthorizationPolicyProvider, PermissionAuthorizationPolicyProvider>();
+    // }
+    //
+    // private static void AddCaching(IServiceCollection services, IConfiguration configuration)
+    // {
+    //     string connectionString = configuration.GetConnectionString("Cache") ??
+    //                               throw new ArgumentNullException(nameof(configuration));
+    //
+    //     services.AddStackExchangeRedisCache(options => options.Configuration = connectionString);
+    //
+    //     services.AddSingleton<ICacheService, CacheService>();
+    // }
+    //
+    // private static void AddHealthChecks(IServiceCollection services, IConfiguration configuration)
+    // {
+    //     services.AddHealthChecks()
+    //         .AddNpgSql(configuration.GetConnectionString("Database")!)
+    //         .AddRedis(configuration.GetConnectionString("Cache")!)
+    //         .AddUrlGroup(new Uri(configuration["KeyCloak:BaseUrl"]!), HttpMethod.Get, "keycloak");
+    // }
+    //
+    // private static void AddApiVersioning(IServiceCollection services)
+    // {
+    //     services
+    //         .AddApiVersioning(options =>
+    //         {
+    //             options.DefaultApiVersion = new ApiVersion(1);
+    //             options.ReportApiVersions = true;
+    //             options.ApiVersionReader = new UrlSegmentApiVersionReader();
+    //         })
+    //         .AddMvc()
+    //         .AddApiExplorer(options =>
+    //         {
+    //             options.GroupNameFormat = "'v'V";
+    //             options.SubstituteApiVersionInUrl = true;
+    //         });
+    // }
+    //
+    // private static void AddBackgroundJobs(IServiceCollection services, IConfiguration configuration)
+    // {
+    //     services.Configure<OutboxOptions>(configuration.GetSection("Outbox"));
+    //
+    //     services.AddQuartz();
+    //
+    //     services.AddQuartzHostedService(options => options.WaitForJobsToComplete = true);
+    //
+    //     services.ConfigureOptions<ProcessOutboxMessagesJobSetup>();
+    // }
 }
